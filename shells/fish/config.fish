82643018e8afb1aa4c6f4733ab7ceaba31f503c0
Fran <fcomrqz@users.noreply.github.com>
2021-11-08 18:19:40 -0600

    feat: select terminal background when open a new window

--- shells/fish/config.fish
+++ shells/fish/config.fish
@@ -2,13 +2,16 @@ starship init fish | source
 
 set -Ux BAT_THEME ansi
 
+set -U fish_prompt_pwd_dir_length 0
 set -U fish_greeting
+
+# Colors
 set -U fish_color_command normal
 set -U fish_color_param bryellow -b
 set -U fish_color_quote brgreen -b
 set -U fish_color_error brred -b
-set -U fish_color_cancel brred -b
-set -U fish_color_autosuggestion white
+set -U fish_color_cancel red -b
+set -U fish_color_autosuggestion brblack
 set -U fish_color_redirection cyan -b
 set -U fish_color_end brmagenta -b
 set -U fish_color_operator brmagenta -b
@@ -21,7 +24,7 @@ set -U fish_color_search_match green
 
 
 # Pager: selected element
-set -U fish_pager_color_selected_background --background=brblack
+set -U fish_pager_color_selected_background --background=brwhite
 set -U fish_pager_color_selected_prefix normal
 set -U fish_pager_color_selected_completion normal
 set -U fish_pager_color_selected_description normal
@@ -30,16 +33,27 @@ set -U fish_pager_color_selected_description normal
 set -U fish_pager_color_background --background=normal
 
 # Pager: prefix string, i.e. the string that is to be completed
-set -U fish_pager_color_prefix white
-set -U fish_pager_color_secondary_prefix white
+set -U fish_pager_color_prefix brblack
+set -U fish_pager_color_secondary_prefix brblack
 
 # Pager: completion itself, i.e. the proposed rest of the string
-set -U fish_pager_color_completion white
-set -U fish_pager_color_secondary_completion white
+set -U fish_pager_color_completion brblack
+set -U fish_pager_color_secondary_completion brblack
 
 # Pager: description
-set -U fish_pager_color_description white
-set -U fish_pager_color_secondary_description white
+set -U fish_pager_color_description brblack
+set -U fish_pager_color_secondary_description brblack
 
 # Pager:  progress bar at the bottom left corner
-set -U fish_pager_color_progress brwhite -b --background=normal
+set -U fish_pager_color_progress white -b --background=normal
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/fran/google-cloud-sdk/path.fish.inc' ]; . '/Users/fran/google-cloud-sdk/path.fish.inc'; end
+
+# Select Terminal Appearance
+defaults read -g AppleInterfaceStyle > /dev/null 2>&1
+if test $status -eq 0
+osascript -e 'tell application "Terminal"
+           set current settings of tabs of windows to settings set "dark" # Theme name
+         end tell'
+end
