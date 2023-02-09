4d874a66acdeea340c54cf50fd85c22e31e80d30
Fran <fcomrqz@users.noreply.github.com>
2023-02-09 17:31:04 -0600

    feat(install): update apps to install

--- install.sh
+++ install.sh
@@ -12,6 +12,15 @@ echo '  Homebrew: macOS Package Manager'
 brew install fish > /dev/null 2>&1
 echo '  Fish Shell: Finally, a command line shell for the 90s'
 
+mkdir ~/.atom
+ln -sF ~/dotfiles/atom/config.cson ~/.atom/config.cson
+ln -sF ~/dotfiles/atom/init.coffee ~/.atom/init.coffee
+ln -sF ~/dotfiles/atom/keymap.cson ~/.atom/keymap.cson
+ln -sF ~/dotfiles/atom/snippets.cson ~/.atom/snippets.cson
+ln -sF ~/dotfiles/atom/styles.less ~/.atom/styles.less
+echo '  [dotfiles]: Atom Linked'
+
+mkdir ~/.config
 mkdir ~/.config/fish
 ln -sF ~/dotfiles/shells/fish/functions ~/.config/fish
 ln -sF ~/dotfiles/shells/fish/config.fish ~/.config/fish/config.fish
@@ -42,15 +51,20 @@ echo '  screen: screen manager with VT100/ANSI terminal emulation'
 brew install cloc > /dev/null 2>&1
 echo '  cloc: Count Lines of Code'
 
-brew install deno > /dev/null 2>&1
-echo '  deno: A secure runtime for JavaScript and TypeScript'
+brew install node > /dev/null 2>&1
+echo "  node(latest): A JavaScript runtime built on Chrome's V8 JavaScript engine"
 
-brew install node@14 > /dev/null 2>&1
-brew link node@14 > /dev/null 2>&1
-echo "  node v14: A JavaScript runtime built on Chrome's V8 JavaScript engine"
+brew install node@16 > /dev/null 2>&1
+echo "  node v16: A JavaScript runtime built on Chrome's V8 JavaScript engine"
+
+brew install node@10 > /dev/null 2>&1
+echo "  node v10: A JavaScript runtime built on Chrome's V8 JavaScript engine"
 
 brew install dog > /dev/null 2>&1
 echo '  dog: DNS client'
+
+brew install httpstat > /dev/null 2>&1
+echo '  httpstat: Curl statistics made simple'
 
 brew install ripgrep > /dev/null 2>&1
 echo '  ripgrep: Better grep'
@@ -80,9 +94,15 @@ echo '  httpie: HTTP client for the API era'
 brew install mas > /dev/null 2>&1
 echo '  mas: AppStore CLI'
 
+brew install sqlite > /dev/null 2>&1
+echo '  sqlite: Command-line interface for SQLite'
+
 brew tap mongodb/brew
-brew install mongodb-community@5.0 > /dev/null 2>&1
+brew install mongodb-community > /dev/null 2>&1
 echo '  mongodb-community: MongoDB server'
+
+brew install mongodb-database-tools > /dev/null 2>&1
+echo '  mongodb-database-tools: standard utilities'
 
 brew install mongosh > /dev/null 2>&1
 echo '  mongosh: MongoDB Shell'
@@ -103,63 +123,23 @@ echo ' [brew]: Installing Desktop Apps'
 brew install --cask google-chrome > /dev/null 2>&1
 echo "  Google Chrome: Google's Web Browser"
 
-brew install --cask firefox > /dev/null 2>&1
-echo "  Firefox: Mozillaz' Web Browser"
-
-brew install --cask atom > /dev/null 2>&1
-echo '  Atom: A hackable text editor for the 21st Century'
-ln -sF ~/dotfiles/atom/config.cson ~/.atom/config.cson
-ln -sF ~/dotfiles/atom/init.coffee ~/.atom/init.coffee
-ln -sF ~/dotfiles/atom/keymap.cson ~/.atom/keymap.cson
-ln -sF ~/dotfiles/atom/snippets.cson ~/.atom/snippets.cson
-ln -sF ~/dotfiles/atom/styles.less ~/.atom/styles.less
-echo '  [dotfiles]: Atom Linked'
-echo '  [apm]: Installing Atom Packages'
-apm i atom-quokka > /dev/null 2>&1
-echo '    atom-quokka: A hackable text editor for the 21st Century'
-apm i atom-wallaby > /dev/null 2>&1
-echo '    atom-wallaby: A hackable text editor for the 21st Century'
-apm i auto-dark-mode > /dev/null 2>&1
-echo '    auto-dark-mode: A hackable text editor for the 21st Century'
-apm i busy-signal > /dev/null 2>&1
-echo '    busy-signal: A hackable text editor for the 21st Century'
-apm i intentions > /dev/null 2>&1
-echo '    intentions: A hackable text editor for the 21st Century'
-apm i keyboard-scroll > /dev/null 2>&1
-echo '    keyboard-scroll: A hackable text editor for the 21st Century'
-apm i language-fish-shell > /dev/null 2>&1
-echo '    language-fish-shell: A hackable text editor for the 21st Century'
-apm i linter > /dev/null 2>&1
-echo '    linter: A hackable text editor for the 21st Century'
-apm i linter-eslint > /dev/null 2>&1
-echo '    linter-eslint: A hackable text editor for the 21st Century'
-apm i linter-ui-default > /dev/null 2>&1
-echo '    linter-ui-default: A hackable text editor for the 21st Century'
-apm i prettier-atom > /dev/null 2>&1
-echo '    prettier-atom: A hackable text editor for the 21st Century'
-ln -sF ~/dotfiles/prettier/.prettierrc ~/.prettierrc
-echo '    [dotfiles]: Prettier Linked'
-apm i smart-tags > /dev/null 2>&1
-echo '    smart-tags: A hackable text editor for the 21st Century'
-apm i fcomrqz/atom-ide-typescript > /dev/null 2>&1
-echo '    fcomrqz/atom-ide-typescript: A hackable text editor for the 21st Century'
-apm i fcomrqz/atom-ide-vue > /dev/null 2>&1
-echo '    fcomrqz/atom-ide-vue: A hackable text editor for the 21st Century'
-apm i fcomrqz/atom-language-vue > /dev/null 2>&1
-echo '    fcomrqz/atom-language-vue: A hackable text editor for the 21st Century'
-
-
 brew install --cask loopback > /dev/null 2>&1
 echo '  Loopback: Cable-Free Audio Routing'
 
 brew install --cask raycast > /dev/null 2>&1
 echo '  Raycast: Better Spotlight'
 
-brew install --cask dash > /dev/null 2>&1
-echo '  Dash: Offline Software Documentation'
-
 brew install --cask adobe-creative-cloud > /dev/null 2>&1
 echo '  Adobe Creative Cloud: Adobe App Manager'
+
+brew install --cask mongodb-compass > /dev/null 2>&1
+echo '  MongoDB Compass: GUI for MongoDB'
+
+brew install --cask proxyman > /dev/null 2>&1
+echo '  Proxyman: Apple Native Web Debugging Proxy'
+
+brew install --cask github > /dev/null 2>&1
+echo '  GitHub Desktop: Desktop client for GitHub repositories'
 
 brew install --cask android-studio > /dev/null 2>&1
 echo '  Android Studio:  Integrated Development Environment for Android'
@@ -178,9 +158,6 @@ echo '  np: A better `npm publish`'
 
 npm i -g lighthouse > /dev/null 2>&1
 echo '  lighthouse: Automated auditing, performance metrics, and best practices for the web.'
-
-npm i -g vercel > /dev/null 2>&1
-echo '  vercel: Develop. Preview. Ship.'
 
 echo ''
 echo '[mas]: Installing apps from AppStore'
