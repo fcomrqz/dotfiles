3b00386db6c072775edbcec4a906dd140f51bbb7
Fran <fcomrqz@users.noreply.github.com>
2023-02-09 20:02:10 -0600

    fix(install): ask sudo once

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+
+mkdir ~/.atom
+ln -sF ~/dotfiles/atom/config.cson ~/.atom/config.cson
+ln -sF ~/dotfiles/atom/init.coffee ~/.atom/init.coffee
+ln -sF ~/dotfiles/atom/keymap.cson ~/.atom/keymap.cson
+ln -sF ~/dotfiles/atom/snippets.cson ~/.atom/snippets.cson
+ln -sF ~/dotfiles/atom/styles.less ~/.atom/styles.less
+
 echo ''
 read -p 'Enter your GitHub Token: ' GITHUB_TOKEN
 export GITHUB_TOKEN=$GITHUB_TOKEN
-sudo -v
 
+echo ''
+echo 'Wait until Homebrew is installed to enter sudo mode'
 echo ''
 echo '[brew]: Installing Packages'
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash -s arg1 arg2  > /dev/null 2>&1
@@ -12,15 +21,11 @@ echo '  Homebrew: macOS Package Manager'
 brew install fish > /dev/null 2>&1
 echo '  Fish Shell: Finally, a command line shell for the 90s'
 
-mkdir ~/.atom
-ln -sF ~/dotfiles/atom/config.cson ~/.atom/config.cson
-ln -sF ~/dotfiles/atom/init.coffee ~/.atom/init.coffee
-ln -sF ~/dotfiles/atom/keymap.cson ~/.atom/keymap.cson
-ln -sF ~/dotfiles/atom/snippets.cson ~/.atom/snippets.cson
-ln -sF ~/dotfiles/atom/styles.less ~/.atom/styles.less
-echo '  [dotfiles]: Atom Linked'
-
 mkdir ~/.config
+brew install starship > /dev/null 2>&1
+ln -sF ~/dotfiles/starship/starship.toml ~/.config/starship.toml
+echo '  starship: The minimal, blazing-fast, and infinitely customizable prompt for any shell'
+
 mkdir ~/.config/fish
 ln -sF ~/dotfiles/shells/fish/functions ~/.config/fish
 ln -sF ~/dotfiles/shells/fish/config.fish ~/.config/fish/config.fish
@@ -28,7 +33,10 @@ ln -sF ~/dotfiles/shells/.bashrc ~/.bashrc
 ln -sF ~/dotfiles/shells/.zshrc ~/.zshrc
 echo '    [dotfiles]: Shells linked'
 
+echo ''
+sudo -v
 sudo sh -c "echo $(which fish) >> /etc/shells"
+echo ''
 
 chsh -s /usr/local/bin/fish
 echo '    [sudo]: Fish shell is the default shell'
@@ -38,6 +46,9 @@ touch ~/.hushlogin
 
 fish ~/dotfiles/tokens.fish
 echo '    [fish]: Tokens are exported'
+
+brew install --cask adobe-creative-cloud > /dev/null 2>&1
+echo '  Adobe Creative Cloud: Adobe App Manager'
 
 brew install bat > /dev/null 2>&1
 mkdir ~/.config/bat
@@ -52,13 +63,9 @@ brew install cloc > /dev/null 2>&1
 echo '  cloc: Count Lines of Code'
 
 brew install node > /dev/null 2>&1
-echo "  node(latest): A JavaScript runtime built on Chrome's V8 JavaScript engine"
-
 brew install node@16 > /dev/null 2>&1
-echo "  node v16: A JavaScript runtime built on Chrome's V8 JavaScript engine"
-
 brew install node@10 > /dev/null 2>&1
-echo "  node v10: A JavaScript runtime built on Chrome's V8 JavaScript engine"
+echo "  node: A JavaScript runtime built on Chrome's V8 JavaScript engine"
 
 brew install dog > /dev/null 2>&1
 echo '  dog: DNS client'
@@ -85,7 +92,7 @@ brew install git-delta > /dev/null 2>&1
 echo '  delta: A better diff'
 
 brew install htop > /dev/null 2>&1
-mdkir ~/.config/htop
+mkdir ~/.config/htop
 ln -sF ~/dotfiles/htop/htoprc ~/.config/htop/htoprc
 echo '  htop: An interactive process viewer'
 
@@ -98,7 +105,7 @@ echo '  mas: AppStore CLI'
 brew install sqlite > /dev/null 2>&1
 echo '  sqlite: Command-line interface for SQLite'
 
-brew tap mongodb/brew
+brew tap mongodb/brew > /dev/null 2>&1
 brew install mongodb-community > /dev/null 2>&1
 echo '  mongodb-community: MongoDB server'
 
@@ -107,10 +114,6 @@ echo '  mongodb-database-tools: standard utilities'
 
 brew install mongosh > /dev/null 2>&1
 echo '  mongosh: MongoDB Shell'
-
-brew install starship > /dev/null 2>&1
-ln -sF ~/dotfiles/starship/starship.toml ~/.config/starship.toml
-echo '  starship: The minimal, blazing-fast, and infinitely customizable prompt for any shell'
 
 brew install trash > /dev/null 2>&1
 echo '  trash: tool that moves files or folder to the trash'
@@ -130,9 +133,6 @@ echo '  Loopback: Cable-Free Audio Routing'
 brew install --cask raycast > /dev/null 2>&1
 echo '  Raycast: Better Spotlight'
 
-brew install --cask adobe-creative-cloud > /dev/null 2>&1
-echo '  Adobe Creative Cloud: Adobe App Manager'
-
 brew install --cask mongodb-compass > /dev/null 2>&1
 echo '  MongoDB Compass: GUI for MongoDB'
 
@@ -148,7 +148,7 @@ echo '  Android Studio:  Integrated Development Environment for Android'
 echo ''
 echo '[npm]: Installing Global Node Packages'
 npm i -g npm@latest --force > /dev/null 2>&1
-echo 'npm: link dotfile'
+echo '    [dotfiles]: npm linked'
 ln -sF ~/dotfiles/npm/.npmrc ~/.npmrc
 
 npm i -g npm-check-updates > /dev/null 2>&1
@@ -171,7 +171,6 @@ echo '  Things: To-do list'
 
 mas install 1482454543 > /dev/null 2>&1
 echo "  Twitter: It's what's happening"
-echo ''
 
 mas install 409203825 > /dev/null 2>&1
 echo "  Numbers: Apple's Spreadsheets"
