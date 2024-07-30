264d6af3dcc42066923dfac913e5987a759c631e
Fran <fcomrqz@users.noreply.github.com>
2024-07-30 17:42:28 -0600

    feat(scripts): links to icloud

---
+++ scripts/links.sh
@@ -0,0 +1,3 @@
+ln -s ~/Library/Mobile\ Documents/com\~apple\~CloudDocs ~/icloud
+ln -s ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Secrets ~/secrets
+ln -s ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Backups ~/backups
