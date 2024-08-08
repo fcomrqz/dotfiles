95fa293ddde08f8b2ad925e77cc8566fa7dfad17
Fran <fcomrqz@users.noreply.github.com>
2024-08-07 18:32:09 -0600

    clean: move code to Developer folder

--- shells/fish/functions/open_project.fish
+++ shells/fish/functions/open_project.fish
@@ -1,7 +1,7 @@
 function open_project
-    set project (command find ~/habive ~/fcomrqz ~/mondraco ~/bookip -type d -maxdepth 1 -mindepth 1 | string replace -r '/Users/fran/' '' | fzf --query "$argv")
+    set project (command find ~/Developer/habive ~/Developer/fcomrqz ~/Developer/mondraco ~/Developer/bookip -type d -maxdepth 1 -mindepth 1 | string replace -r '/Users/fran/Developer/' '' | fzf --query "$argv")
     if test -n "$project"
-        cd ~/$project
+        cd ~/Developer/$project
     end
     commandline -f repaint
 end
