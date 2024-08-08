8d5ea0a915496e264f14401e95179fcf1e26173c
Fran <fcomrqz@users.noreply.github.com>
2024-08-07 18:30:31 -0600

    feat(fish): minimal shell title

--- shells/fish/functions/open_gitui.fish
+++ shells/fish/functions/open_gitui.fish
@@ -1,5 +1,6 @@
 function open_gitui
     if test -d .git
+        echo -ne "\033]0;gitui · $(echo (basename $PWD))\a"
         command gitui
         commandline -f clear-screen
         commandline -f repaint
--- shells/fish/functions/open_htop.fish
+++ shells/fish/functions/open_htop.fish
@@ -2,8 +2,10 @@ function open_htop
     commandline -f cancel-commandline
     read -l process_name -p "set_color green; echo -n 'htop → '; set_color normal;"
     if test -n "$process_name"
+        echo -ne "\033]0;htop · $(echo $process_name) · $(echo (basename $PWD))\a"
         htop -F $process_name
     else
+        echo -ne "\033]0;htop · $(echo (basename $PWD))\a"
         htop
     end
     commandline -f clear-screen
