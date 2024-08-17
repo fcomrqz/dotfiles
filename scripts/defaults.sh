6341feed55ad9967a01a76586cefd8517dd6e0af
Fran <fcomrqz@users.noreply.github.com>
2024-08-17 13:04:50 -0600

    docs(defaults): improve comments

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -2,15 +2,14 @@
 defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
 defaults write com.apple.Finder "AppleShowAllFiles" -bool "true"
 
-# Menu Bar
+# Hide Menu Bar
 defaults write NSGlobalDomain _HIHideMenuBar -bool true
 
-# Hide Dock
+# Auto hide Dock
 defaults write com.apple.dock autohide -bool true && killall Dock
-defaults write com.apple.dock autohide-delay -float 1000 && killall Dock
 defaults write com.apple.dock no-bouncing -bool TRUE && killall Dock
 
-# Do not create `.DS_Store` files
+# Do not create `.DS_Store` files in Network Drives
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 
 # Omit crash report dialog {none|basic|developer|server}
