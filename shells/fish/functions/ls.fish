6f4a8e1735bcdce090c9db5625816197ed1b38ca
Fran <fcomrqz@users.noreply.github.com>
2024-08-17 13:11:54 -0600

    feat(fish/ls): remove tree characters

--- shells/fish/functions/ls.fish
+++ shells/fish/functions/ls.fish
@@ -1,3 +1,3 @@
 function ls -d 'List files and directories'
-    command tree -C -a --dirsfirst -L 1 -I '.git|.DS_Store|node_modules|package-lock.json|Creative\ Cloud\ Files' $argv
+    command tree -i -C -a --dirsfirst -L 1 -I '.git|.DS_Store|node_modules|Creative\ Cloud\ Files' $argv
 end
