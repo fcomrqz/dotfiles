7ee91bc973648ef9096d22e45e25669844a2d4b4
Fran <fcomrqz@users.noreply.github.com>
2025-04-22 14:12:43 -0600

    fix(tailscale): update launch daemon path to latest version

--- tailscale/setup.fish
+++ tailscale/setup.fish
@@ -1,5 +1,8 @@
 ln ~/Developer/fcomrqz/dotfiles/tailscale/tailscale.plist /Library/LaunchDaemons/com.tailscale.tailscaled.plist
 launchctl load /Library/LaunchDaemons/com.tailscale.tailscaled.plist
+chmod 644 /Library/LaunchDaemons/com.tailscale.tailscaled.plist
+chown root:wheel /Library/LaunchDaemons/com.tailscale.tailscaled.plist
+
 fdesetup list -extended
 fdesetup status -extended
 fdesetup authrestart -extended
