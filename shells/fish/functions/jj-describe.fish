5e240a24210c7bff9c8119236add0eba851c684c
Fran <fcomrqz@users.noreply.github.com>
2025-04-23 17:04:32 -0600

    feat(jujutsu): update commit types

--- shells/fish/functions/jj-describe.fish
+++ shells/fish/functions/jj-describe.fish
@@ -4,17 +4,17 @@ function jj-describe
         return 1
     end
 
-    set type (gum filter "fix" "feat" "test" "clean" "perf" "build" "ci" "dx" "style" "workflow" "chore" "types" "release" "docs" --placeholder="" --prompt="→ " --height 6 --prompt.foreground 2 --indicator.foreground 4 --match.foreground 4)
+    set types feat fix test clean perf build ci release types docs style
+    set type (gum filter $types --placeholder="" --prompt="→ " --height 6 --placeholder "" --prompt "→ " --prompt.foreground 2 --indicator '▌' --indicator.foreground 4 --match.foreground 4 --no-strip-ansi)
 
     if test -z "$type"
         echo -en "\033[5A\033[J"
-        promptError "Describe aborted"
+        promptError "describe aborted"
         return 1
     end
 
     set prompt (set_color green)" → "(set_color reset)
 
-    # Read the scope with native fish read
     read -P "$type$prompt" scope
 
     if test -n "$scope"
@@ -29,7 +29,7 @@ function jj-describe
         echo -en "\033[5A\033[J"
     else
         echo -en "\033[1A\r\033[K"
-        promptError "Describe aborted"
+        promptError "description missing"
     end
 
     commandline -f repaint
