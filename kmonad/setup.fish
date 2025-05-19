698e66f9a40051823089b7f7ebd046d18115e288
Fran <fcomrqz@users.noreply.github.com>
2025-05-19 00:43:37 -0600

    fix(kmonad): launch daemon

--- kmonad/setup.fish
+++ kmonad/setup.fish
@@ -1,2 +1,4 @@
+ln ~/Developer/fcomrqz/dotfiles/kmonad/karabiner.plist ~/Library/LaunchAgents/com.fcomrqz.karabiner.plist
 ln ~/Developer/fcomrqz/dotfiles/kmonad/kmonad.plist ~/Library/LaunchAgents/com.fcomrqz.kmonad.plist
+launchctl load ~/Library/LaunchAgents/com.fcomrqz.karabiner.plist
 launchctl load ~/Library/LaunchAgents/com.fcomrqz.kmonad.plist
