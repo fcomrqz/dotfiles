20753e2496ad0da6525945a215e96aab5af2807d
Fran <fcomrqz@users.noreply.github.com>
2025-10-15 23:18:58 -0600

    feat: update Nicole's keyboard

--- qmk/wt60/build.sh
+++ qmk/wt60/build.sh
@@ -3,8 +3,6 @@
 qmk config user.keyboard=wilba_tech/wt60_h1
 qmk config user.keymap=fcomrqz
 
-ln -sF ~/Developer/fcomrqz/dotfiles/qmk/wt60/keymap.c ~/qmk_firmware/keyboards/wilba_tech/wt60_h1/keymaps/fcomrqz/
-ln -sF ~/Developer/fcomrqz/dotfiles/qmk/wt60/config.h ~/qmk_firmware/keyboards/wilba_tech/wt60_h1/keymaps/fcomrqz/
-ln -sF ~/Developer/fcomrqz/dotfiles/qmk/wt60/rules.mk ~/qmk_firmware/keyboards/wilba_tech/wt60_h1/keymaps/fcomrqz/
+ln -sF ~/Developer/fcomrqz/dotfiles/qmk/wt60 ~/qmk_firmware/keyboards/wilba_tech/wt60_h1/keymaps/fcomrqz
 
 qmk flash
